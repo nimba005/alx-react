3de6854c7bd8e359b852745807b33ed58fe359de
nimba005 <nimbaoyugi@gmail.com>
2024-04-02 17:35:38 +0300

    task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+  $('body').append('<h2>Holberton Dashboard</h2>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
