f219ec0ebb450c28a7e16d0904e7a9ae5de1e014
nimba005 <nimbaoyugi@gmail.com>
2024-04-08 20:02:48 +0300

    props

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Notifications } from './Notifications';
-import reportWebVitals from './reportWebVitals';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React with Webpack!</h1>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-    <div id="root-notifications">
-      <Notifications />
-    </div>
-  </>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
